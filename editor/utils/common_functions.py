b1d77a1e3591a0496b20650bab80fe605219f03b
Peter Clark <peterc1092@gmail.com>
2021-02-13 19:11:00 +0000

    PlayerAttribute class added
    PlayerAttributeOption helper also added, along with more functions in common_functions.py.
    
    Signed-off-by: Peter Clark <peterc1092@gmail.com>

--- editor/utils/common_functions.py
+++ editor/utils/common_functions.py
@@ -1,3 +1,6 @@
+import random
+
+
 def bytes_to_int(ba, a):
     ia = [ba[a + i] for i in range(4)]
     return ia[0] | (ia[1] << 8) | (ia[2] << 16) | (ia[3] << 24)
@@ -5,3 +8,29 @@ def bytes_to_int(ba, a):
 
 def zero_fill_right_shift(val, n):
     return (val % 0x100000000) >> n
+
+
+def string_to_code_value(val):
+    return val.lower().replace(" ", "_")
+
+
+def get_base_byte_value(b, bf):
+    return (b // bf) * bf
+
+
+def get_lowest_byte_value(b, bf):
+    bb = get_base_byte_value(b, bf)
+    lb = b - bb
+    return lb
+
+
+def round_down(n, d):
+    return n - (n % d)
+
+
+def get_random_value_from_list(l):
+    return random.choice(l)
+
+
+def replace_tuple_at_index(t, i, v):
+    return t[:i] + (v,) + t[i + 1 :]
